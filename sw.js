2395e2a1315d0eb3c1d7d17cccb3329430dc300e
Rob Wiebe <rjw.ywam@gmail.com>
2019-06-20 16:31:04 -1000

    trying out some segment data

--- sw.js
+++ sw.js
@@ -1,2 +1,39 @@
 // service worker already registered through gatsby
-// only need to put it to work from this file
+// only need to put it to work from this file 
+console.log('this is my registered service worker')
+self.addEventListener('install', (event) => {
+    console.log('Service Worker] Installing Service Worker ...', event)
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {
+                console.log('[Service Worker] pre-caching app shell')
+                // cache.add('/index.html')
+                // cache.add('/') 
+                cache.addAll([
+                    '/index.html',
+                    '/',
+                    // '/*.js',
+                    // '*.json',
+                    '/404.html',
+                    // '/styles.*.css'
+                ])
+            })
+    )
+})
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker] Activating Service Worker ...', event)
+    return self.clients.claim()
+})
+self.addEventListener('fetch', (event) => {
+    console.log('Service Worker] fetching something ...', event)
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
